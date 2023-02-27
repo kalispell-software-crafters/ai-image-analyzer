ebdc97be29137e1e47222404130db491a4f51ad7
Travis Craft <tlcraft@users.noreply.github.com>
2023-02-26 18:58:59 -0700

    Flesh out initial flow.

--- src/api/index.py
+++ src/api/index.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from src.services.video_service import download_video
+from src.classes.analyze_video_response import AnalyzeVideoResponse
 from src.services.image_analysis_service import run_image_analysis
+from src.services.video_service import download_video
 
 app = FastAPI()
 
@@ -9,8 +10,8 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/process_video")
-async def process_video(video_url: str, target_item: str):
-    download_video(video_url)
-    run_image_analysis()
-    return {"video_url": video_url, "target_item": target_item }
+@app.post("/analyze_video")
+async def analyze_video(video_url: str, target_item: str) -> AnalyzeVideoResponse:
+    video_data = download_video(video_url)
+    results = run_image_analysis(video_data)
+    return AnalyzeVideoResponse(video_url=video_url, target_item=target_item, results=results)
