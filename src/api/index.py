5fad7dce4db06e90cf6914f4091c4cdc72b3a563
Victor Calderon <2266703+vcalderon2009@users.noreply.github.com>
2023-03-04 10:26:25 -0600

    Fixing typo in 'Makefile' and updating set of package requirements. Also code-linting source code

--- src/api/index.py
+++ src/api/index.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from src.services.video_service import download_video
+
 from src.services.image_analysis_service import run_image_analysis
+from src.services.video_service import download_video
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
@@ -13,4 +15,4 @@ async def root():
 async def process_video(video_url: str, target_item: str):
     download_video(video_url)
     run_image_analysis()
-    return {"video_url": video_url, "target_item": target_item }
+    return {"video_url": video_url, "target_item": target_item}
